59976d6da2b949328cde18aa9cdfacc9706297b6
Nicolai Waniek <rochus@rochus.net>
2023-09-08 21:46:55 +0200

    support up to 64 items in NCR_COUNT_ARGS

--- include/ncr/ncr_utils.hpp
+++ include/ncr/ncr_utils.hpp
@@ -113,13 +113,16 @@ constexpr std::size_t len(T(&)[N]) { return N; }
 
 
 /*
- * used to suppress compilation warnings for unused variables. Also helps to
- * explicitly mark them as unused. Provided are also macros for up to 10
- * arguments / variables that are not used.
+ * Count the number of arguments to a variadic macro. Up to 64 arguments are
+ * supported
  */
-#define NCR_COUNT_ARGS2(X,X10,X9,X8,X7,X6,X5,X4,X3,X2,X1,N,...) N
-#define NCR_COUNT_ARGS(...) NCR_COUNT_ARGS2(0, __VA_ARGS__ ,10,9,8,7,6,5,4,3,2,1,0)
+#define NCR_COUNT_ARGS2(X,_64,_63,_62,_61,_60,_59,_58,_57,_56,_55,_54,_53,_52,_51,_50,_49,_48,_47,_46,_45,_44,_43,_42,_41,_40,_39,_38,_37,_36,_35,_34,_33,_32,_31,_30,_29,_28,_27,_26,_25,_24,_23,_22,_21,_20,_19,_18,_17,_16,_15,_14,_13,_12,_11,_10,_9,_8,_7,_6,_5,_4,_3,_2,_1,N,...) N
+#define NCR_COUNT_ARGS(...) NCR_COUNT_ARGS2(0, __VA_ARGS__ ,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1)
 
+/*
+ * Suppress warnings for unused arguments. Up to 10 arguments are supported in
+ * the variadic version NCR_UNUSED
+ */
 #define NCR_UNUSED_1(X)        (void)X;
 #define NCR_UNUSED_2(X0, X1)   NCR_UNUSED_1(X0); NCR_UNUSED_1(X1)
 #define NCR_UNUSED_3(X0, ...)  NCR_UNUSED_1(X0); NCR_UNUSED_2(__VA_ARGS__)
